1013ec28fa2af81a63f2629e31ef7c0e3a9e289f
JulesNyom <julesnyom@gmail.com>
2024-08-25 14:54:02 +0200

    Creation of the project. Basic setup with Next.js, Tailwind CSS, and TypeScript.

---
+++ feeded_app/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: "FeedEd",
+  description: "FeedEd est une application web conçue pour faciliter les enquêtes de satisfaction pour les formateurs et les organismes de formation. Simplifiez la collecte de feedback, améliorez vos programmes et offrez une meilleure expérience d'apprentissage.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
