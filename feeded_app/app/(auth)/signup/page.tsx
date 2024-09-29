1e32ac2e7cd9277d5173e490802615e8ec469dc8
JulesNyom <julesnyom@gmail.com>
2024-09-29 16:35:40 +0200

    modified the design of the FormManagement component

--- feeded_app/app/(auth)/signup/page.tsx
+++ feeded_app/app/(auth)/signup/page.tsx
@@ -1,8 +1,10 @@
-import SignUp from '@/components/shared/authentication/SignUp'
+import SignUp from "@/components/shared/authentication/SignUp";
 
 export default function EnhancedSignUpPage() {
   return (
-<div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-100 to-indigo-100 p-4">      <SignUp />
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-100 to-indigo-100 p-4">
+      {" "}
+      <SignUp />
     </div>
-  )
+  );
 }
